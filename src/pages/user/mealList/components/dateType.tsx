03200628634bf42916ee71f76ea4b0d776798d79
strong6208 <hamilton6208@gmail.com>
2023-04-07 15:50:26 -0700

    [feat] add date type page

--- src/pages/user/mealList/components/dateType.tsx
+++ src/pages/user/mealList/components/dateType.tsx
@@ -1,19 +1,22 @@
-import React, { useState } from "react"
+import React from "react"
 import "dayjs/locale/ja"
 
 import { AdapterDayjs, Box, DatePicker, LocalizationProvider, TextField } from "src/UILibrary"
 
-export const DateType: React.FC = () => {
-  const [date, setDate] = useState<string | null>()
+interface DateTypeProps {
+  mealDate: string
+  setMealDate?: Function
+}
 
+export const DateType: React.FC<DateTypeProps> = ({ mealDate, setMealDate }) => {
   return (
     <Box sx={{ display: "flex", justifyContent: { md: "flex-start", xs: "center" } }}>
       <Box sx={{ display: { md: "block", xs: "none" } }}>
         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ja">
           <DatePicker
             views={["month", "year"]}
-            value={date}
-            onChange={(value) => setDate(value)}
+            value={mealDate || ""}
+            onChange={(value) => !!setMealDate && setMealDate(value)}
             renderInput={(params) => <TextField fullWidth sx={{ width: "160px" }} {...params} />}
           />
         </LocalizationProvider>
@@ -22,8 +25,8 @@ export const DateType: React.FC = () => {
         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ja">
           <DatePicker
             views={["month", "day", "year"]}
-            value={date}
-            onChange={(value) => setDate(value)}
+            value={mealDate || ""}
+            onChange={(value) => !!setMealDate && setMealDate(value)}
             renderInput={(params) => <TextField fullWidth sx={{ width: "160px" }} {...params} />}
           />
         </LocalizationProvider>

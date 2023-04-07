38f469b1b6e9ecffdbbed811b1b8babe2e69ec48
strong6208 <hamilton6208@gmail.com>
2023-04-07 15:51:24 -0700

    [feat] meal detail table components

--- src/pages/user/mealList/components/mealDetail.tsx
+++ src/pages/user/mealList/components/mealDetail.tsx
@@ -1,13 +1,14 @@
 import React from "react"
 import { useTranslation } from "react-i18next"
 
-import { Typography } from "src/UILibrary"
 import { FieldDefinition } from "src/components/mealTable"
 import { Modal } from "src/components/modal"
+import { DialogTitle, IconButton, Typography } from "src/UILibrary"
+import { MealDetailTable } from "src/components/mealDetailTable"
+import { CloseIcon } from "src/assets/icons/CloseIcon"
 
 import { MealDetail } from "src/types/mealDetail"
 import { MOCK_MEAL_DETAIL_DATA } from "./mockMedalDetail"
-import { MealDetailTable } from "src/components/mealDetailTable"
 
 const mealDetailData: MealDetail[] = MOCK_MEAL_DETAIL_DATA
 
@@ -15,6 +16,7 @@ interface ApplicationModalProps {
   open: boolean
   // eslint-disable-next-line no-unused-vars
   handleMealDetailOpen: (open: boolean) => void
+  mealDate: string
 }
 
 const fields: FieldDefinition<MealDetail>[] = [
@@ -44,8 +46,12 @@ const fields: FieldDefinition<MealDetail>[] = [
 export const MealDetailModal: React.FC<ApplicationModalProps> = ({
   open,
   handleMealDetailOpen,
+  mealDate,
 }) => {
   const { t } = useTranslation()
+  const getYear = new Date(mealDate).getFullYear()
+  const getMonth = new Date(mealDate).getMonth()
+  const getDay = new Date(mealDate).getDate()
 
   const handleClose = () => {
     handleMealDetailOpen(false)
@@ -53,9 +59,26 @@ export const MealDetailModal: React.FC<ApplicationModalProps> = ({
 
   return (
     <Modal handleClose={handleClose} open={open}>
-      <Typography.Heading sx={{ fontWeight: 500, textAlign: "center" }}>
-        {t("application.application")}
-      </Typography.Heading>
+      <DialogTitle sx={{ display: "flex", justifyContent: "center", position: "relative" }}>
+        <Typography.Title sx={{ color: "text.secondary", fontWeight: 400 }}>
+          {`${getYear}年${getMonth + 1}月${getDay}日`}
+        </Typography.Title>
+        <Typography.Title sx={{ color: "text.secondary", fontWeight: 400 }}>
+          {t("meal.breakfast")}
+        </Typography.Title>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: "absolute",
+            color: "secondary.dark",
+            top: 0,
+            right: "0.5rem",
+          }}
+        >
+          <CloseIcon width="18" height="18" />
+        </IconButton>
+      </DialogTitle>
       <MealDetailTable fields={fields} content={mealDetailData} />
     </Modal>
   )

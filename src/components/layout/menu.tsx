8e79c352ae54c1cd3d8e4dc23fa1bcedc770ffed
strong6208 <hamilton6208@gmail.com>
2023-04-16 04:54:50 -0700

    valid label

--- src/components/layout/menu.tsx
+++ src/components/layout/menu.tsx
@@ -34,7 +34,28 @@ export const MenuLayout: FC<PropsWithChildren> = ({ children }) => {
   const location = useLocation()
   const navigate = useNavigate()
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null)
-  const [menus, setMenus] = useState<Label[]>()
+  const [menus, setMenus] = useState<Label[]>([
+    {
+      icon: UserIcon,
+      label: "menu.my_page",
+      path: "/my-page",
+    },
+    {
+      label: "menu.from_parent",
+      icon: UsersIcon,
+      path: "/application",
+    },
+    {
+      label: "menu.meal",
+      icon: MealIcon,
+      path: "/meal-list",
+    },
+    {
+      label: "menu.pdf_transfer",
+      icon: BuildingIcon,
+      path: "/pdf-transfer",
+    },
+  ])
 
   const onOpenMobileMenu = (e: MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(e.currentTarget)
